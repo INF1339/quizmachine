531cc1519b1d3d177bbd3e198ce7c3524c575fad
Dan Ryan <daniel.ryan@utoronto.ca>
2019-12-13 11:49:36 -0500

    Add files via upload

---
+++ experimentalquiz2.js
@@ -0,0 +1,168 @@
+var q = {
+  "questestinterop": {
+    "assessment": {
+      "ident": "geec09e63fae9f27e331250de2bcbb036",
+      "title": "Experimental Quiz",
+      "section": {
+        "ident": "root_section",
+        "item": {
+          "ident": "g135c5f9f86f22af56fda510555fb8418",
+          "title": "ILOVETRASH",
+          "itemmetadata": {
+            "qtimetadata": {
+              "qtimetadatafield": [
+                {
+                  "fieldlabel": "question_type",
+                  "fieldentry": "multiple_choice_question"
+                },
+                {
+                  "fieldlabel": "points_possible",
+                  "fieldentry": "1.0"
+                },
+                {
+                  "fieldlabel": "original_answer_ids",
+                  "fieldentry": "4810,7500,1441,3260"
+                },
+                {
+                  "fieldlabel": "assessment_question_identifierref",
+                  "fieldentry": "g9bdd518458eacbc4bf95e6d1fad2a60b"
+                }
+              ]
+            }
+          },
+          "presentation": {
+            "material": {
+              "mattext": {
+                "texttype": "text/html",
+                "text": "<div><p>What is the name of the character who sings I love trash?</p><p>VIDEO HERE</p></div>"
+              }
+            },
+            "response_lid": {
+              "ident": "response1",
+              "rcardinality": "Single",
+              "render_choice": {
+                "response_label": [
+                  {
+                    "ident": "4810",
+                    "material": {
+                      "mattext": {
+                        "texttype": "text/plain",
+                        "text": "Oscar"
+                      }
+                    }
+                  },
+                  {
+                    "ident": "7500",
+                    "material": {
+                      "mattext": {
+                        "texttype": "text/plain",
+                        "text": "Grover"
+                      }
+                    }
+                  },
+                  {
+                    "ident": "1441",
+                    "material": {
+                      "mattext": {
+                        "texttype": "text/plain",
+                        "text": "Big Bird"
+                      }
+                    }
+                  },
+                  {
+                    "ident": "3260",
+                    "material": {
+                      "mattext": {
+                        "texttype": "text/plain",
+                        "text": "Buffy"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          },
+          "itemfeedback": [
+            {
+              "ident": "general_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>Maybe more television?</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "correct_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>GOOD!</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "general_incorrect_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>BAD!</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "4810_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>Yes indeed it was Oscar!</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "7500_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>Why would you say Grover?</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "1441_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>It might be true of BB, but they did not sing it here.</p>"
+                  }
+                }
+              }
+            },
+            {
+              "ident": "3260_fb",
+              "flow_mat": {
+                "material": {
+                  "mattext": {
+                    "texttype": "text/html",
+                    "text": "<p>Wrong show maybe?</p>"
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+}
