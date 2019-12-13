d6687d496c656605cd8be29983bdb4ef30a80852
Dan Ryan <daniel.ryan@utoronto.ca>
2019-12-12 20:05:09 -0500

    Update experimentalquiz.js

--- experimentalquiz.js
+++ experimentalquiz.js
@@ -43,8 +43,11 @@ var q = {
             "material": {
               "mattext": {
                 "texttype": "text/html",
-                "text": "<div><p>What is the name of the character who sings I love trash?</p><p>VIDEO HERE</p></div>"
-              }
+                "text": "<div><p>What is the name of the character who sings I love trash?</p>
+                <p>VIDEO HERE<iframe width='560' height='315' src='https://www.youtube.com/embed/mUXojQ_nhD4' 
+                frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+                allowfullscreen></iframe></p></div>"
+               }
             },
             "response_lid": {
               "ident": "response1",
